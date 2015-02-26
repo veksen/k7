701419ddf46ec79beb30545c4ba2b1bc9fe0deb5
veksen <jp@veksen.net>
2015-02-26 18:23:45 -0500

    add local and remote environments

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local'  => array('veksen-XPS13-9333'),
+	'remote' => array('veksen-linode')
 
 ));
 
