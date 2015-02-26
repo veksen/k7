6e598c1ef40bc25a46ae264f43321e47c97820a4
veksen <jp@veksen.net>
2015-02-26 18:28:45 -0500

    base entries migration

---
+++ app/database/migrations/2015_02_26_232505_create_entries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('entries', function($t) {
+			$t->increments('id');
+			$t->string('idea', 255);
+			$t->string('email', 255);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('entries');
+	}
+
+}
