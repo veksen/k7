af170b6a4d6d864bf7e204c75387a42b3f0855a9
veksen <jp@veksen.net>
2015-02-27 09:04:25 -0500

    base master view, bootstrap & jquery

---
+++ app/views/master.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>k7</title>
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+@yield('main')
+<script src="/js/jquery-1.11.1.min.js"></script>
+</body>
+</html>
