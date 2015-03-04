fd180f652a2a6a53c1944b1556f6b92353a946a0
veksen <jp@veksen.net>
2015-03-04 18:09:44 -0500

    add material floating label for possible use

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+$(function() {
+	$('.form-control').on('focus blur', function (e) {
+	$(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+}).trigger('blur');
+});
