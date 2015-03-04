3b73fc317f1183bee761fa70e5d9e9a6a5aff5a0
veksen <jp@veksen.net>
2015-03-04 00:53:13 -0500

    base style + more rules styling

--- app/views/entry/rules.blade.php
+++ app/views/entry/rules.blade.php
@@ -2,52 +2,66 @@
 
 @section('main')
 
-<div class="well">
-
 <h2>Règlements du concours</h2>
 
 <h3><b>Objet du concours :</b> Qui sommes-nous ? Et pourquoi le concours ?</h3>
 
-<p>Afin d’être éligible au concours vous devez vous assurer de nous fournir</p>
-<ol>
-	<li>votre nom complet</li>
-	<li>une adresse</li>
-	<li>un numéro de téléphone valide</li>
-	<li>la date à laquelle vous participer</li>
-	<li>être âgée de 18 ans et plus</li>
-	<li>être citoyen canadien</li>
-</ol>
+<div class="well">
+	<p>Afin d’être éligible au concours vous devez vous assurer de nous fournir</p>
+	<ol>
+		<li>votre nom complet</li>
+		<li>une adresse</li>
+		<li>un numéro de téléphone valide</li>
+		<li>la date à laquelle vous participer</li>
+		<li>être âgée de 18 ans et plus</li>
+		<li>être citoyen canadien</li>
+	</ol>
+</div>
 
-<h3>Dates à retenir :</h3>
-<ul>
-	<li>Période du concours : x</li>
-	<li>Évaluation et sélection des participants : x</li>
-	<li>Notification des gagnants : x</li>
-	<li>Annonce publique des gagnants : x</li>
-</ul>
+<div class="well">
+	<h3>Dates à retenir :</h3>
+	<ul>
+		<li>Période du concours : x</li>
+		<li>Évaluation et sélection des participants : x</li>
+		<li>Notification des gagnants : x</li>
+		<li>Annonce publique des gagnants : x</li>
+	</ul>
+</div>
 
-<p>Prix : X</p>
-<p>le prix doit être accepté tel quel et n’est ni échangeable, ni transférable, ni monnayable.</p>
+<div class="row">
+	<div class="col-xs-10 col-xs-push-1">
+		<p>Prix : X</p>
+		<p>le prix doit être accepté tel quel et n’est ni échangeable, ni transférable, ni monnayable.</p>
+	</div>
+</div>
 
 <hr>
 
-<p><span class="count">1-</span> L'organisateur du concours se réserve le droit, à son entière discrétion, d'annuler, de terminer, de modifier ou de suspendre le présent concours en tout ou en partie dans l'éventualité où se manifesteraient un événement, une erreur ou toute intervention humaine pouvant corrompre ou affecter l'administration, la sécurité, l'impartialité ou le déroulement du concours</p>
+<div class="well">
+	<p><span class="count">1</span> L'organisateur du concours se réserve le droit, à son entière discrétion, d'annuler, de terminer, de modifier ou de suspendre le présent concours en tout ou en partie dans l'éventualité où se manifesteraient un événement, une erreur ou toute intervention humaine pouvant corrompre ou affecter l'administration, la sécurité, l'impartialité ou le déroulement du concours</p>
 
-<p><span class="count">2-</span> En participant à ce concours ou en tentant d’y participer, chaque participant et (ou) prétendu participant accepte d’exonérer, de décharger et de tenir indemnes, pour toujours, les Partenaires ainsi que leurs administrateurs, dirigeants, employés, ou autres représentants de tout(e) réclamation, action, dommage, demande, moyen d’action, poursuite, dette, indemnité, contrat ou responsabilité de quelque nature que ce soit découlant ou liée à la participation au concours ou à la tentative de participation du participant, le respect ou non de ce règlement de concours ou l’acceptation et l’utilisation du prix.</p>
+	<p><span class="count">2</span> En participant à ce concours ou en tentant d’y participer, chaque participant et (ou) prétendu participant accepte d’exonérer, de décharger et de tenir indemnes, pour toujours, les Partenaires ainsi que leurs administrateurs, dirigeants, employés, ou autres représentants de tout(e) réclamation, action, dommage, demande, moyen d’action, poursuite, dette, indemnité, contrat ou responsabilité de quelque nature que ce soit découlant ou liée à la participation au concours ou à la tentative de participation du participant, le respect ou non de ce règlement de concours ou l’acceptation et l’utilisation du prix.</p>
 
-<p><span class="count">3-</span> Tout frais encourus par le gagnant demeure à la charge exclusive du gagnant.</p>
+	<p><span class="count">3</span> Tout frais encourus par le gagnant demeure à la charge exclusive du gagnant.</p>
 
-<p><span class="count">4-</span> L'organisateur du concours, les partenaires du concours, leurs compagnies affiliées, leurs employés se dégagent aussi de toute responsabilité pour tout dommage ou toute perte pouvant être causée, directement ou indirectement, en tout ou en partie, par le téléchargement de toute page Internet, de tout logiciel ou de tout formulaire et par la transmission de toute information visant la participation au présent concours.</p>
+	<p><span class="count">4</span> L'organisateur du concours, les partenaires du concours, leurs compagnies affiliées, leurs employés se dégagent aussi de toute responsabilité pour tout dommage ou toute perte pouvant être causée, directement ou indirectement, en tout ou en partie, par le téléchargement de toute page Internet, de tout logiciel ou de tout formulaire et par la transmission de toute information visant la participation au présent concours.</p>
 
-<p><span class="count">5-</span> Tous les formulaires de participation deviennent la propriété de l'organisateur du concours et aucune communication ne sera échangée avec les participants dans le cadre du présent concours autrement que conformément au présent règlement ou à l'initiative de l'organisateur du concours.</p>
+	<p><span class="count">5</span> Tous les formulaires de participation deviennent la propriété de l'organisateur du concours et aucune communication ne sera échangée avec les participants dans le cadre du présent concours autrement que conformément au présent règlement ou à l'initiative de l'organisateur du concours.</p>
 
-<p><span class="count">6-</span> L'organisateur du concours se réserve le droit de disqualifier toute personne ou d'annuler la participation de toute personne participant à ce concours ou tentant d'y participer par un moyen contraire à ce règlement. Selon la nature du geste posé, cette personne pourrait être référée aux autorités judiciaires compétentes le cas échéant.</p>
+	<p><span class="count">6</span> L'organisateur du concours se réserve le droit de disqualifier toute personne ou d'annuler la participation de toute personne participant à ce concours ou tentant d'y participer par un moyen contraire à ce règlement. Selon la nature du geste posé, cette personne pourrait être référée aux autorités judiciaires compétentes le cas échéant.</p>
 
-<p><span class="count">7-</span> Toute personne participant consent, par sa participation au concours, à ce que son nom et sa photo soient utilisés à des fins publicitaires, en faveur de l'organisateur du concours ou des partenaires du concours, sans rémunération.</p>
+	<p><span class="count">7</span> Toute personne participant consent, par sa participation au concours, à ce que son nom et sa photo soient utilisés à des fins publicitaires, en faveur de l'organisateur du concours ou des partenaires du concours, sans rémunération.</p>
+</div>
 
-<p>L'organisateur du concours se réserve le droit d'éliminer à son gré tous les bulletins de participation expédiés en envoi multiple (spamming) et ceux dont la provenance est douteuse.</p>
+<div class="row">
+	<div class="col-xs-10 col-xs-push-1">
+		<p>L'organisateur du concours se réserve le droit d'éliminer à son gré tous les bulletins de participation expédiés en envoi multiple (spamming) et ceux dont la provenance est douteuse.</p>
+	</div>
+</div>
 
-<p><span class="count">1-</span> *un nom original de votre cru, disponible en .com ou .ca  ? (reformuler)</p>
+<div class="well">
+	<p><span class="count">1</span> *un nom original de votre cru, disponible en .com ou .ca  ? (reformuler)</p>
+</div>
 
 </div>
 
