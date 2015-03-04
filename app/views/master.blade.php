694969eb1d9de24c3f22b514a9c3d31d1ab8eeb8
veksen <jp@veksen.net>
2015-03-04 00:07:16 -0500

    base nav & style

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -7,6 +7,29 @@
 	<link href="/css/main.css" rel="stylesheet">
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+        <span class="sr-only">Afficher le menu</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">k7 media</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Participer <span class="sr-only">(page courante)</span></a></li>
+        <li><a href="#">Règlements</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Admin</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <div class="container">
 @if(Session::has('message'))
 <div class="message">
