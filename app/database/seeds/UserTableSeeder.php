509df1af018239679104e7fec28ab071c0d0ee6c
veksen <jp@veksen.net>
2015-03-06 09:12:43 -0500

    user table seed

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+class UserTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+
+		$user = new User;
+		$user->email = 'jp@veksen.net';
+		$user->password = Hash::make('m0np4s5');
+		$user->save();
+
+		$user = new User;
+		$user->email = 'bab_thegreat@hotmail.com';
+		$user->password = Hash::make('fr4k7');
+		$user->save();
+	}
+
+}
