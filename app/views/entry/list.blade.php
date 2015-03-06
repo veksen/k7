18c4065bcd4c6ef4a5388ad64ff57263f22da416
veksen <jp@veksen.net>
2015-03-06 18:16:51 -0500

    rename partial list row template

--- app/views/entry/list.blade.php
+++ app/views/entry/list.blade.php
@@ -22,25 +22,25 @@
 	<tbody>
 		@foreach($entries as $entry)
 		@if($entry->status == '1')
-		@include('entry._listfields')
+		@include('entry._listrow')
 		@endif
 		@endforeach
 
 		@foreach($entries as $entry)
 		@if($entry->status == '0')
-		@include('entry._listfields')
+		@include('entry._listrow')
 		@endif
 		@endforeach
 
 		@foreach($entries as $entry)
 		@if($entry->status == '2')
-		@include('entry._listfields')
+		@include('entry._listrow')
 		@endif
 		@endforeach
 
 		@foreach($entries as $entry)
 		@if($entry->status == '3')
-		@include('entry._listfields')
+		@include('entry._listrow')
 		@endif
 		@endforeach
 	</tbody>
