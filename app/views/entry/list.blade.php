635e89eb4d5cb7c44f608978d4b49b23f2beca58
veksen <jp@veksen.net>
2015-03-06 09:16:08 -0500

    add space between firstname and lastname

--- app/views/entry/list.blade.php
+++ app/views/entry/list.blade.php
@@ -26,7 +26,7 @@
 			<td>{{ $entry->id }}</td>
 			<td>{{ $entry->idea }}</td>
 			<td>{{ $entry->email }}</td>
-			<td>{{ $entry->firstname . $entry->lastname }}</td>
+			<td>{{ $entry->firstname . ' ' . $entry->lastname }}</td>
 			<td><a href="{{ route('entries.tag', array($entry->id, 1)) }}">bon</a></td>
 			<td><a href="{{ route('entries.tag', array($entry->id, 2)) }}">dsq</a></td>
 			<td><a href="{{ route('entries.tag', array($entry->id, 3)) }}">del</a></td>
