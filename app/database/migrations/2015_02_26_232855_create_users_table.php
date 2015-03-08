34cfd8406b0673c66f0365ef5d42e8f4dad63069
veksen <jp@veksen.net>
2015-03-08 03:17:28 -0400

    fix users migration

--- app/database/migrations/2015_02_26_232855_create_users_table.php
+++ app/database/migrations/2015_02_26_232855_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration {
 			$t->increments('id');
 			$t->string('email', 255);
 			$t->string('password', 255);
-			$t->string('remember_token', 100)
+			$t->string('remember_token', 100);
 			$t->timestamps();
 		});
 	}
